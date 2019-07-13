68a8ab644eb5eae3bb0b113849d8f9729dd607cd
cramosc <cramoscuevas@gmail.com>
2019-07-13 10:45:16 +0200

    parse jobs sections

--- scripts/build.py
+++ scripts/build.py
@@ -10,25 +10,35 @@ ROOT_DIR = dirname(dirname(abspath(__file__)))
 SECTIONS_DIR = join(ROOT_DIR, '_sections')
 JSON_FILE = join(ROOT_DIR, 'dist', 'l10n.json')
 
+
 def main():
-    l10n = {'es': {}, 'de': {}}
+    l10n = {'es': {'jobs': []}, 'de': {'jobs': []}}
     for file in os.listdir(SECTIONS_DIR):
         if file.endswith('.json'):
             with open(os.path.join(SECTIONS_DIR, file), 'r') as f:
                 section=json.load(f)
                 key = section['title']
 
+                menu = section[f'menu_{lan}']
+                title = section.get(f'title_{lan}', '')
+                body = section.get(f'body_{lan}', '')
+                body = markdown2.markdown(body.strip())
+
                 for lan in 'de', 'es':
-                    l10n[lan][key] = section[f'menu_{lan}']
-                    if f'title_{lan}' in section:
-                        l10n[lan][f'{key}_title'] = section[f'title_{lan}']
-                    if f'body_{lan}' in section:
-                        body = section[f'body_{lan}']
-                        body = markdown2.markdown(body.strip())
+                    if key.startswith('job_'):
+                        l10n[lan]['jobs'].append({
+                            'menu': menu,
+                            'title': title,
+                            'body': body
+                        })
+                    else:
+                        l10n[lan][key] = menu
+                        l10n[lan][f'{key}_title'] = title
                         l10n[lan][f'{key}_body'] = body
 
     with open(JSON_FILE, 'w', encoding='utf-8') as f:
         f.write(json.dumps(l10n, ensure_ascii=False))
+
 
 if __name__ == '__main__':
     sys.exit(main())

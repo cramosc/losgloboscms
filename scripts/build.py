c1603ad86fa9a5c6e9da85110251dfcc2bd37550
Carlos Ramos Cuevas <cramoscuevas@gmail.com>
2020-11-03 07:11:22 +0100

    create jsons for new page

--- scripts/build.py
+++ scripts/build.py
@@ -2,6 +2,9 @@ import json
 import sys
 import os
 import markdown2
+import frontmatter
+import yaml
+from collections import defaultdict
 
 from os.path import dirname, abspath, join
 
@@ -11,8 +14,41 @@ SECTIONS_DIR = join(ROOT_DIR, '_sections')
 JSON_FILE = join(ROOT_DIR, 'dist', 'l10n.json')
 JS_FILE = join(ROOT_DIR, 'dist', 'l10n.js')
 
+CONTENT_DIR = join(ROOT_DIR, 'content')
+CONTENT_DEST_DIR = join(ROOT_DIR, 'dist', 'content')
+
+def create_new_pages():
+    for lan in ['de', 'es']:
+        directory = join(CONTENT_DIR, 'pages', lan)
+        sitemap = defaultdict(list)
+
+        for file in os.listdir(directory):
+            path = os.path.splitext(file)[0].split('_')
+            page = frontmatter.load(join(directory, file))
+            sitemap['/'.join(path[:-1])].append((page.metadata['order'], path[-1], page.metadata['menu']))
+            json_str = json.dumps({'data': page.metadata, 'content': markdown2.markdown(page.content, extras=["tables"])}, ensure_ascii=False)
+            file_dir = join(CONTENT_DEST_DIR, lan, *path[:-1])
+            os.makedirs(file_dir, exist_ok=True)
+            with open(join(file_dir, path[-1] + '.json'), 'w') as f:
+                f.write(json_str)
+
+        sitemap = {k: [a[1:] for a in sorted(v, key=lambda a: a[0])] for k, v in sitemap.items()}
+        with open(join(CONTENT_DEST_DIR, lan, 'sitemap.json'), 'w') as f:
+                f.write(json.dumps(sitemap, ensure_ascii=False))
+
+        lang = {'de': 'german', 'es': 'spanish'}[lan]
+        with open(join(CONTENT_DIR, 'layout.yml'), 'r') as f:
+            data = {k: v[lang] for k, v in yaml.safe_load(f).items()}
+        with open(join(CONTENT_DEST_DIR, lan, 'layout.json'), 'w') as f:
+            f.write(json.dumps(data, ensure_ascii=False))
+        with open(join(CONTENT_DIR, 'news.yml'), 'r') as f:
+            data = [markdown2.markdown(n[lang], extras=["tables"]) for n in yaml.safe_load(f)['news']]
+        with open(join(CONTENT_DEST_DIR, lan, 'news.json'), 'w') as f:
+            f.write(json.dumps(data, ensure_ascii=False))
 
 def main():
+    create_new_pages()
+    
     l10n = {'es': {'job_sections': {}}, 'de': {'job_sections': {}}}
     for file in os.listdir(SECTIONS_DIR):
         if file.endswith('.json'):

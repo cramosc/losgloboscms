60249ac912005d7f9ab666bc2cd63c98cde83dd8
Carlos Ramos Cuevas <cramoscuevas@gmail.com>
2020-11-10 12:30:47 +0100

    activate deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 cd out
-find * -type f -exec echo Deploying {} ... \; -exec curl -s -u "$FTP_CREDENTIALS" --ftp-create-dirs -T {} "$FTP_SERVER"/test/{} \; -exec echo Done \;
+find * -type f -exec echo Deploying {} ... \; -exec curl -s -u "$FTP_CREDENTIALS" --ftp-create-dirs -T {} "$FTP_SERVER"/{} \; -exec echo Done \;
 cd -

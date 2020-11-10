83a6863c23fe06d4533de333e7d10fdb05f70420
Carlos Ramos Cuevas <cramoscuevas@gmail.com>
2020-11-10 11:06:00 +0100

    add condition to action [skip ci]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 cd out
-find * -type f -exec curl -u "$FTP_CREDENTIALS" --ftp-create-dirs -T {} "$FTP_SERVER"/test/{} \;
+find * -type f -exec echo Deploying {} ... \; -exec curl -s -u "$FTP_CREDENTIALS" --ftp-create-dirs -T {} "$FTP_SERVER"/test/{} \; -exec echo Done \;
 cd -

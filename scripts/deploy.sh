1cd04e858c7e5cfd9b142b47d1fb2294412f755d
Carlos Ramos Cuevas <cramoscuevas@gmail.com>
2020-11-11 09:48:58 +0100

    add logging to deploy script [deploy]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,17 @@ cd out
 tar -cvzf "../$OUT_FILE" *
 cd ..
 
+echo "uploading tar file..."
 curl -s -u "$FTP_CREDENTIALS" -T "$OUT_FILE" "$FTP_SERVER/$OUT_FILE"
+echo "uploading php script..."
 curl -s -u "$FTP_CREDENTIALS" -T scripts/extractor.php "$FTP_SERVER/extractor.php"
 
-curl -s "http://losglobos.de/extractor.php"
+echo "executing php script..."
+curl "http://losglobos.de/extractor.php"
 
-curl -s -u "$FTP_CREDENTIALS" "$FTP_SERVER" -Q 'DELE out.tar.gz'
-curl -s -u "$FTP_CREDENTIALS" "$FTP_SERVER" -Q 'DELE out.tar'
-curl -s -u "$FTP_CREDENTIALS" "$FTP_SERVER" -Q 'DELE extractor.php'
+# echo "clean up..."
+# curl -s -u "$FTP_CREDENTIALS" "$FTP_SERVER" -Q 'DELE out.tar.gz'
+# curl -s -u "$FTP_CREDENTIALS" "$FTP_SERVER" -Q 'DELE out.tar'
+# curl -s -u "$FTP_CREDENTIALS" "$FTP_SERVER" -Q 'DELE extractor.php'
+
+echo "Done"
